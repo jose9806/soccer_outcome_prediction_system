3dabec51b38ea3893b0e76c082d7f362ea73b1e0
jose9806 <jose.d98@hotmail.com>
2025-03-01 23:51:42 -0500

    Remove deprecated feature extraction files and add new feature extraction script

--- src/data_processing/transformers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Transformers for normalizing and standardizing soccer match data.
-"""
-
-from .stat_normalizer import StatNormalizer
-from .team_standardizer import TeamStandardizer
-
-__all__ = ["StatNormalizer", "TeamStandardizer"]
--- src/data_processing/validators/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Validators for checking the quality and consistency of soccer match data.
-"""
-
-from .schema_validator import SchemaValidator
-from .consistency_checker import ConsistencyChecker
-
-__all__ = ["SchemaValidator", "ConsistencyChecker"]
